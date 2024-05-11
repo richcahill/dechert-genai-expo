8f3328d0bcede1cc8d2fa5b35ec5eee124170f96
Rich Cahill <rich@rc3.me>
2024-05-11 17:59:45 +0100

    update sidebar

--- src/app/sketch/page.tsx
+++ src/app/sketch/page.tsx
@@ -7,9 +7,14 @@ import { Button } from "@/components/ui/button";
 import NavBar from "@/components/nav-bar";
 import SideBar from "@/components/sidebar";
 import Icon from "@/components/icon";
-import { Loader, Pencil, Wand } from "lucide-react";
+import { Camera, Loader, Pencil, Wand } from "lucide-react";
 
 const styles = [
+  {
+    title: "Pastel Color Illustration",
+    prompt:
+      "Create an illustration where the style should be minimalistic with a subdued colour palette, clean lines, and a hand-drawn, sketchy feel but with chunky lines. The illustration should have perspective that gives a sense of depth, and the overall tone should be inviting and informative. Use a limited color scheme with soft hues and ensure that the lines are not too sharp. The illustration should be of ",
+  },
   {
     title: "Pastel Color Illustration",
     prompt:
@@ -31,14 +36,15 @@ const EMPTY_IMG =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjOHPmzH8ACDADZKt3GNsAAAAASUVORK5CYII=";
 
 export default function WebcamPage() {
-  const [inProgress, setInProgress] = useState(false);
+  const [sketchInProgress, setSketchInProgress] = useState(false);
+  const [photoInProgress, setPhotoInProgress] = useState(false);
   const [prompt, setPrompt] = useState<string>(
     "a group of people sitting around a campfire in the woods"
   );
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-  const getImage = async () => {
-    setInProgress(true);
+  const getSketch = async () => {
+    setSketchInProgress(true);
 
     let sketchPrompt = styles[0].prompt + prompt;
 
@@ -50,7 +56,24 @@ export default function WebcamPage() {
       body: JSON.stringify({ prompt: sketchPrompt }),
     });
     const data = await response.json();
-    setInProgress(false);
+    setSketchInProgress(false);
+    setImageUrl(data[0].url);
+  };
+
+  const getImage = async () => {
+    setPhotoInProgress(true);
+
+    let sketchPrompt = styles[3].prompt + prompt;
+
+    const response = await fetch("/api/openai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: sketchPrompt }),
+    });
+    const data = await response.json();
+    setPhotoInProgress(false);
     setImageUrl(data[0].url);
   };
 
@@ -82,11 +105,28 @@ export default function WebcamPage() {
             <Button
               className="h-full aspect-square"
               onClick={() => {
+                getSketch();
+              }}
+              disabled={sketchInProgress}
+            >
+              {sketchInProgress ? (
+                <Loader className="animate-spin" />
+              ) : (
+                <Pencil />
+              )}
+            </Button>
+            <Button
+              className="h-full aspect-square"
+              onClick={() => {
                 getImage();
               }}
-              disabled={inProgress}
+              disabled={photoInProgress}
             >
-              {inProgress ? <Loader className="animate-spin" /> : <Pencil />}
+              {photoInProgress ? (
+                <Loader className="animate-spin" />
+              ) : (
+                <Camera />
+              )}
             </Button>
           </div>
         </div>
@@ -95,6 +135,7 @@ export default function WebcamPage() {
           title="No Pen Illustration"
           description={[
             "Type in the details of a drawing you would like the model to create for you in a matter of seconds.",
+            "Click the pencil for a sketch or the camera for a realistic photo.",
             "If you are not happy with the results, add or edit your instructions and try again.",
             "This model was developed and successfully used by Dechert's Innovation Certificate Program 2024 participants to generate illustrations for their innovation projects.",
           ]}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -22,7 +22,6 @@ const SideBar = ({
   return (
     <div className="h-full bg-white border rounded-md flex flex-col justify-between p-6">
       <div className="flex flex-col gap-4">
-        <p className="text-xl opacity-25">#{number}</p>
         <h2 className="font-medium text-4xl">{name}</h2>
         <div className="space-y-2">
           {description.map((desc, i) => (
@@ -31,8 +30,8 @@ const SideBar = ({
         </div>
         {thought && (
           <div className="bg-zinc-100 border-zinc-300 border p-4 flex flex-col gap-2 rounded">
-            <div className="flex justify-start gap-2">
-              <h3 className="font-medium text-xl">Food For Thought</h3>
+            <div className="flex justify-between gap-2">
+              <h3 className="font-medium text-xl flex-1">Food For Thought</h3>
               <Pizza />
             </div>
             <p className="text-lg">{thought}</p>
@@ -41,8 +40,8 @@ const SideBar = ({
       </div>
       {tip && (
         <div className="bg-zinc-50 border-zinc-200 border p-4 flex flex-col gap-2 rounded">
-          <div className="flex justify-start gap-2">
-            <h3 className="font-medium text-xl">Tip</h3>
+          <div className="flex justify-between gap-2">
+            <h3 className="font-medium text-xl flex-1">Tip</h3>
             <Lightbulb />
           </div>
           <p className="text-lg">{tip}</p>
